8739af0fe8e43152b06ae9eaf1999b93425a850c
Konstantin Korikov <lostclus@gmail.com>
2023-03-09 19:18:36 +0000

    Add motor speed.

--- fpvrobotrpiserver/static/control.js
+++ fpvrobotrpiserver/static/control.js
@@ -6,6 +6,9 @@ const DEVICE_CAM_SERVO_MOVE_H = 5;
 const DEVICE_CAM_SERVO_MOVE_V = 6;
 const DEVICE_VOLAGE = 7;
 
+const MOTOR_SPEED_MIN = 70;
+const MOTOR_SPEED_MAX = 255;
+const MOTOR_SPEED_INC = 5;
 const VOLTAGE_MEASUREMENTS = 5;
 
 const wsAddr = (document.location.href + 'ws').replace(/^http/, 'ws');
@@ -13,6 +16,7 @@ const socket = new WebSocket(wsAddr);
 
 var lastKeyEventName = null;
 var lastKeyCode = null;
+var motorSpeed = MOTOR_SPEED_MIN;
 var voltageArray = [];
 
 
@@ -36,6 +40,14 @@ function sendPing() {
     socket.send(payload);
 }
 
+function motorSpeedUp() {
+    motorSpeed = Math.min(motorSpeed + MOTOR_SPEED_INC, MOTOR_SPEED_MAX);
+}
+
+function motorSpeedMin() {
+    motorSpeed = MOTOR_SPEED_MIN;
+}
+
 function requestVoltage() {
     sendDevMsg(DEVICE_VOLAGE, 0);
 }
@@ -47,26 +59,32 @@ function requestVoltageAsync() {
 }
 
 function onKeyEvent(eventName, event) {
+    /*
     if (eventName == lastKeyEventName && event.code == lastKeyCode) {
        return;
     }
 
     lastKeyEventName = eventName;
     lastKeyCode = event.code;
+    */
 
     var isUp = eventName == "keyup";
     var isDown = eventName == "keydown";
 
     if (event.code == "KeyW") {
-        sendDevMsg(DEVICE_MOTOR_L, isDown ? 100 : 0);
-        sendDevMsg(DEVICE_MOTOR_R, isDown ? 100 : 0);
+        sendDevMsg(DEVICE_MOTOR_L, isDown ? motorSpeed : 0);
+        sendDevMsg(DEVICE_MOTOR_R, isDown ? motorSpeed : 0);
+        if (isDown) {motorSpeedUp();} else {motorSpeedMin();}
     } else if (event.code == "KeyS") {
-        sendDevMsg(DEVICE_MOTOR_L, isDown ? -100 : 0);
-        sendDevMsg(DEVICE_MOTOR_R, isDown ? -100 : 0);
+        sendDevMsg(DEVICE_MOTOR_L, isDown ? -motorSpeed : 0);
+        sendDevMsg(DEVICE_MOTOR_R, isDown ? -motorSpeed : 0);
+        if (isDown) {motorSpeedUp();} else {motorSpeedMin();}
     } else if (event.code == "KeyA") {
-        sendDevMsg(DEVICE_MOTOR_R, isDown ? 100 : 0);
+        sendDevMsg(DEVICE_MOTOR_R, isDown ? motorSpeed : 0);
+        if (isDown) {motorSpeedUp();} else {motorSpeedMin();}
     } else if (event.code == "KeyD") {
-        sendDevMsg(DEVICE_MOTOR_L, isDown ? 100 : 0);
+        sendDevMsg(DEVICE_MOTOR_L, isDown ? motorSpeed : 0);
+        if (isDown) {motorSpeedUp();} else {motorSpeedMin();}
     } else if (event.code == "ArrowLeft") {
         sendDevMsg(DEVICE_CAM_SERVO_MOVE_H, isDown ? 1 : 0);
     } else if (event.code == "ArrowRight") {
@@ -75,7 +93,12 @@ function onKeyEvent(eventName, event) {
         sendDevMsg(DEVICE_CAM_SERVO_MOVE_V, isDown ? 1 : 0);
     } else if (event.code == "ArrowUp") {
         sendDevMsg(DEVICE_CAM_SERVO_MOVE_V, isDown ? -1 : 0);
-    } else if (event.code == "Escape") {
+    } else if (event.code == "Home") {
+        sendDevMsg(DEVICE_CAM_SERVO_MOVE_H, 0);
+        sendDevMsg(DEVICE_CAM_SERVO_MOVE_V, 0);
+        sendDevMsg(DEVICE_CAM_SERVO_H, 90);
+        sendDevMsg(DEVICE_CAM_SERVO_V, 90);
+    } else if (event.code == "Space") {
         sendDevMsg(DEVICE_MOTOR_L, 0);
         sendDevMsg(DEVICE_MOTOR_R, 0);
         sendDevMsg(DEVICE_CAM_SERVO_MOVE_H, 0);
